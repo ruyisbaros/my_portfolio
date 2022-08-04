2aeb72b2ff644a02697109d56f6eeebcfcd5e2ca
ahmet <ahmet.erdonmez77@gmail.com>
2022-08-04 20:04:02 +0200

    testimonials U Isetted

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,27 @@
 import './testimonials.css'
+import { testimonials } from "../../data"
+
 
 const Testimonials = () => {
     return (
         <section id="testimonials">
-            Testimonials
+            <h4>Review From Professionals</h4>
+            <h2>Testimonials</h2>
+            <div className="container testimonials__container">
+                {
+                    testimonials.map(tst => (
+                        <article key={tst.id} className="testimonial">
+                            <div className="client__avatar">
+                                <img src={tst.image} alt="img" />
+                            </div>
+                            <h5 className="client__name">{tst.name}</h5>
+                            <h5 className="client__prof">{tst.profession}</h5>
+                            <small className="client__review">{tst.comment}</small>
+
+                        </article>
+                    ))
+                }
+            </div>
         </section>
     )
 }
