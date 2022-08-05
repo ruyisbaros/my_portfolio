ee72f383fc4ea58393e89221df889de3f1c49404
ahmet <ahmet.erdonmez77@gmail.com>
2022-08-05 19:55:40 +0200

    page is done :)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithubSquare } from 'react-icons/fa'
+
 import './footer.css'
 
 const Footer = () => {
     return (
         <footer>
-            Footer
+            <a href="#" className="footer__logo">@HMET</a>
+            <ul className="footer__links">
+                <li><a href="#header">Home</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://www.linkedin.com/in/ahmet-erdonmez-085bb8141/" target="_blank" rel="noreferrer">
+                    <BsLinkedin />
+                </a>
+                <a href="https://github.com/ruyisbaros" target="_blank" rel="noreferrer">
+                    <FaGithubSquare />
+                </a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; Ahmet Erdonmez 2022. All rights reserved.</small>
+            </div>
         </footer>
     )
 }
