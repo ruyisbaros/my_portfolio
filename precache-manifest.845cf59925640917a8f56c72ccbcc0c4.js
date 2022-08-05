f84b0e2663c271e1c45416b4433739544f1b0d2c
ahmet <ahmet.erdonmez77@gmail.com>
2022-08-05 20:27:10 +0200

    Updates

---
+++ precache-manifest.845cf59925640917a8f56c72ccbcc0c4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d10fe87b9fddb1cf2b56",
+    "url": "/my_portfolio/static/css/main.31a962bc.chunk.css"
+  },
+  {
+    "revision": "d10fe87b9fddb1cf2b56",
+    "url": "/my_portfolio/static/js/main.d10fe87b.chunk.js"
+  },
+  {
+    "revision": "e8d262ba9d06c2f6bb12",
+    "url": "/my_portfolio/static/js/1.e8d262ba.chunk.js"
+  },
+  {
+    "revision": "dcf3931afee046ce6d05",
+    "url": "/my_portfolio/static/js/runtime~main.dcf3931a.js"
+  },
+  {
+    "revision": "649cd22cb115fddf9a33fe27269478ab",
+    "url": "/my_portfolio/static/media/ahmet_erdonmez_CV.649cd22c.pdf"
+  },
+  {
+    "revision": "e98e4c9d82f49f83d3682d8fc6759601",
+    "url": "/my_portfolio/static/media/ahmet.e98e4c9d.jpg"
+  },
+  {
+    "revision": "a9bdf69e7f6416a39d87f05446f8883b",
+    "url": "/my_portfolio/static/media/a2.a9bdf69e.jpg"
+  },
+  {
+    "revision": "f10fff9a98c60dd4ffd04d00e4cea517",
+    "url": "/my_portfolio/static/media/f1.f10fff9a.png"
+  },
+  {
+    "revision": "fde7ad585faffaff07135d74144356a2",
+    "url": "/my_portfolio/static/media/f2.fde7ad58.png"
+  },
+  {
+    "revision": "4236b0ee0d6ac741a618ae117424bdde",
+    "url": "/my_portfolio/static/media/f4.4236b0ee.png"
+  },
+  {
+    "revision": "48cea1fe415293b9802084c367dd2036",
+    "url": "/my_portfolio/static/media/lace.48cea1fe.jpg"
+  },
+  {
+    "revision": "2a53c103e3866bb9b8914274ac9e1de2",
+    "url": "/my_portfolio/static/media/mithu.2a53c103.jpg"
+  },
+  {
+    "revision": "d9e414f3e8446b08483f3d023b08b3e0",
+    "url": "/my_portfolio/static/media/hisham.d9e414f3.jpg"
+  },
+  {
+    "revision": "a5319c44271e499179c5ba2e9084148d",
+    "url": "/my_portfolio/static/media/thomas.a5319c44.jpg"
+  },
+  {
+    "revision": "d592f5d52d2d14c65b33e5915bad7996",
+    "url": "/my_portfolio/static/media/amr.d592f5d5.jpg"
+  },
+  {
+    "revision": "1190034e13ccc14a0ab89c8d3528b6e0",
+    "url": "/my_portfolio/static/media/jahid.1190034e.jpeg"
+  },
+  {
+    "revision": "3e2f4819eed227df039a8a3d0dcd82d0",
+    "url": "/my_portfolio/index.html"
+  }
+];
